35a36afd95365145dd519b20e075240f1877fc57
hmcvay <hayleymcvay8@gmail.com>
2022-04-01 09:59:26 -0700

    Add animal object to model

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,12 @@
+namespace AnimalShelter
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Species { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Sex { get; set; }
+    public string Description { get; set; }
+  }
+}
