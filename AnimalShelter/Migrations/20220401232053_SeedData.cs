56d9e57808638246ad407d39a604be90d345b9ef
hmcvay <hayleymcvay8@gmail.com>
2022-04-01 16:21:36 -0700

    Add seed Data

---
+++ AnimalShelter/Migrations/20220401232053_SeedData.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Description", "Name", "Sex", "Species" },
+                values: new object[,]
+                {
+                    { 1, 7, "Bites ankles to beg for food", "Matilda", "F", "Cat" },
+                    { 2, 1, "This kitten hides in plastic bags", "Caserole", "M", "Cat" },
+                    { 3, 4, "He only eats hotdogs with ketchup", "Nathan", "M", "Dog" },
+                    { 4, 12, "She's on her way out, tbh", "Brenda", "F", "Dog" },
+                    { 5, 1, "She stares at herself in the reflection", "Toothpick", "F", "Cat" },
+                    { 6, 1, "She can barely eat a kibble she's so smol", "Sea Shell Girl", "F", "Dog" },
+                    { 7, 3, "Will takes bites out of butter if left on the counter", "Butter", "M", "Dog" },
+                    { 8, 2, "Loves to nap in dog beds", "Oyster", "F", "Cat" },
+                    { 9, 2, "Just your basic golden retriever", "Chad", "M", "Cat" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 9);
+        }
+    }
+}
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -10,5 +10,22 @@ namespace AnimalShelter.Models
 
     }
     public DbSet<Animal> Animals { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Animal>()
+        .HasData(
+          new Animal { AnimalId = 1, Species = "Cat", Name = "Matilda", Age = 7, Sex = "F", Description = "Bites ankles to beg for food" },
+          new Animal { AnimalId = 2, Species = "Cat", Name = "Caserole", Age = 1, Sex = "M", Description = "This kitten hides in plastic bags" },
+          new Animal { AnimalId = 3, Species = "Dog", Name = "Nathan", Age = 4, Sex = "M", Description = "He only eats hotdogs with ketchup" },
+          new Animal { AnimalId = 4, Species = "Dog", Name = "Brenda", Age = 12, Sex = "F", Description = "She's on her way out, tbh" },
+          new Animal { AnimalId = 5, Species = "Cat", Name = "Toothpick", Age = 1, Sex = "F", Description = "She stares at herself in the reflection" },
+          new Animal { AnimalId = 6, Species = "Dog", Name = "Sea Shell Girl", Age = 1, Sex = "F", Description = "She can barely eat a kibble she's so smol" },
+          new Animal { AnimalId = 7, Species = "Dog", Name = "Butter", Age = 3, Sex = "M", Description = "Will takes bites out of butter if left on the counter" },
+          new Animal { AnimalId = 8, Species = "Cat", Name = "Oyster", Age = 2, Sex = "F", Description = "Loves to nap in dog beds" },
+          new Animal { AnimalId = 9, Species = "Cat", Name = "Chad", Age = 2, Sex = "M", Description = "Just your basic golden retriever" }
+        );
+    }
+
   }
 }
